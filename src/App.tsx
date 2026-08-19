3be1649303c670fc490b4fb949a05ba2415a86b9
Ilya <cernyhila69@gmail.com>
2026-08-19 19:57:12 +0300

    Плавное появление секций теперь повторяется при каждом пролистывании вверх-вниз

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,8 @@ export default function App() {
         for (const entry of entries) {
           if (entry.isIntersecting) {
             entry.target.classList.add('is-visible')
-            observer.unobserve(entry.target)
+          } else {
+            entry.target.classList.remove('is-visible')
           }
         }
       },
